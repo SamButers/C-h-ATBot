713753051b71baff36a783fc647edb094fd69c1d
Sam Buters <sambuters@gmail.com>
2019-04-01 20:44:53 -0300

    Adding predictions

--- nlu_model.py
+++ nlu_model.py
@@ -1,6 +1,7 @@
 from rasa_nlu.training_data import load_data
 from rasa_nlu.model import Trainer
 from rasa_nlu import config
+from rasa_nlu.model import Interpreter
 
 def train_nlu(data, config_path, model_dir):
 	training_data = load_data(data);
@@ -9,6 +10,12 @@ def train_nlu(data, config_path, model_dir):
 	trainer.train(training_data);
 	
 	model_directory = trainer.persist(model_dir, fixed_model_name = 'catnlu');
+
+def run_nlu():
+	interpreter = Interpreter.load('./models/nlu/default/catnlu');
 	
+	print(interpreter.parse(u"Show me information on the Persian cat"));
+
 if __name__ == '__main__':
 	train_nlu('./data/data.json', 'config_spacy.json', './models/nlu');
+	run_nlu();
